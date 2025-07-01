67b726a64546e66fa59b0030af1d27a4468b28a8
Malachi Burke <malachi.burke@gmail.com>
2025-07-01 08:15:32 -0700

    refactor: cleanup

--- src/estd/internal/variant/visit.h
+++ src/estd/internal/variant/visit.h
@@ -8,71 +8,66 @@
 #if __cplusplus >= 201703L
 namespace estd { namespace internal {
 
-// NOTE: Could have done constexpr if for different R types.  Specialization felt more
+// NOTE: Could have done "constexpr if" for different R types.  Specialization felt more
 // natural, and we're prepped for c++11 compatibility (lean on estd::variadic in that case)
 
 template <class R>
-struct variant_visit
+struct index_visitor
 {
     // DEBT: Upgrade variadic visit to do fold expressions (when c++17)
     template <class Visitor, class Index, size_t ...Is>
-    static constexpr R visit_index(Visitor&& visitor, Index index, index_sequence<Is...>, bool* found)
+    static constexpr R visit(Visitor&& visitor, Index index, index_sequence<Is...>, bool* found)
     {
         using return_type = R;
         return_type result;
         *found = ((Is == index ? (result = visitor(in_place_index<Is>), true) : false) || ...);
         return result;
     }
-
-    template <class Visitor, class Variant, size_t ...Is>
-    static constexpr R visit(Visitor&& visitor, Variant&& vv, index_sequence<Is...>, bool* found)
-    {
-        return visit_index([&](auto vi)
-            {
-                return visitor(*get_ll<vi.index>(std::forward<Variant>(vv)));
-            },
-            vv.index(), index_sequence<Is...>{}, found);
-    }
 };
 
+
 template <>
-struct variant_visit<void>
+struct index_visitor<void>
 {
     // DEBT: Upgrade variadic visit to do fold expressions (when c++17)
     template <class Visitor, class Index, size_t ...Is>
-    static constexpr void visit_index(Visitor&& visitor, Index index, index_sequence<Is...>, bool* found)
+    static constexpr void visit(Visitor&& visitor, Index index, index_sequence<Is...>, bool* found)
     {
         *found = ((Is == index ? (visitor(in_place_index<Is>), true) : false) || ...);
     }
+};
 
+template <class R>
+struct variant_visit
+{
     template <class Visitor, class Variant, size_t ...Is>
-    static constexpr void visit(Visitor&& visitor, Variant&& vv, index_sequence<Is...>, bool* found)
+    static constexpr R visit(Visitor&& visitor, Variant&& vv, index_sequence<Is...>)
     {
-        const int index = vv.index();
-        *found = ((Is == index ? (visitor(*get_ll<Is>(std::forward<Variant>(vv))), true) : false) || ...);
+        [[maybe_unused]] bool found;
+        return index_visitor<R>::visit([&](auto vi)
+            {
+                return visitor(*get_ll<vi.index>(std::forward<Variant>(vv)));
+            },
+            vv.index(), index_sequence<Is...>{}, &found);
     }
 };
 
 template <class Visitor, class ...Types, class R>
 constexpr R visit(Visitor&& visitor, variant<Types...>& vv)
 {
-    [[maybe_unused]] bool found;
-    using indices = index_sequence_for<Types...>;
     return variant_visit<R>::visit(
         std::forward<Visitor>(visitor),
         vv,
-        indices{}, &found);
+        index_sequence_for<Types...>{});
 }
 
 template <class Visitor, class ...Types, class R>
 constexpr R visit(Visitor&& visitor, const variant<Types...>& vv)
 {
-    [[maybe_unused]] bool found;
-    using indices = index_sequence_for<Types...>;
     return variant_visit<R>::visit(
         std::forward<Visitor>(visitor),
         vv,
-        indices{}, &found);
+        index_sequence_for<Types...>{});
 }
 
 }
